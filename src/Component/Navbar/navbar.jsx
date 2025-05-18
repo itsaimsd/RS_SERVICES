a8d37a943ccbd5135030811f39b5bcae4521a8d1
itsaimsd <madhusudantiwari2003@gmail.com>
2025-05-19 03:07:12 +0530

    making center navbar

--- src/Component/Navbar/navbar.jsx
+++ src/Component/Navbar/navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
               href="https://wa.me/7739002652"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-green-500 text-white px-4 py-2 rounded-xl hover:bg-green-600 transition text-center mb-65"
+              className="bg-green-500 text-white px-4 py-2 rounded-xl hover:bg-green-600 transition text-center mb-85"
               onClick={() => setIsOpen(false)}
             >
               WhatsApp
